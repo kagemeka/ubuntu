5a6680e578866736cd3e35724354d4bd54b009b6
kagemeka <kagemeka1@gmail.com>
2022-02-08 11:02:08 +0900

    refactor: directory structure

---
+++ scripts/install_docker_components.sh
@@ -0,0 +1,6 @@
+./docker/install_docker_compose.sh
+./docker/install_nvidia_docker.sh
+./docker/enable_docker_buildkit.sh
+./docker/set_user_group.sh
+./docker/newgrp.sh
+./docker/enable_docker_buildkit.sh
